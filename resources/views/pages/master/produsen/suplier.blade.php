518803e32a54654352c10c51f7afeb5ac07a805c
Yasa Maulana <89472474+yasamaulana@users.noreply.github.com>
2023-08-19 02:29:06 +0700

    typo

--- resources/views/pages/master/produsen/suplier.blade.php
+++ resources/views/pages/master/produsen/suplier.blade.php
@@ -28,7 +28,7 @@
                                 </div>
                                 <div data-tw-merge class="block sm:flex items-center mt-3">
                                     <label data-tw-merge for="horizontal-form-1" class="inline-block mb-2  sm:w-32">
-                                        Kode BPOm
+                                        Kode BPOM
                                     </label>
                                     <input data-tw-merge id="horizontal-form-1" type="text"
                                         class="disabled:bg-slate-100 disabled:cursor-not-allowed dark:disabled:bg-darkmode-800/50 dark:disabled:border-transparent [&amp;[readonly]]:bg-slate-100 [&amp;[readonly]]:cursor-not-allowed [&amp;[readonly]]:dark:bg-darkmode-800/50 [&amp;[readonly]]:dark:border-transparent transition duration-200 ease-in-out w-full text-sm border-slate-200 shadow-sm rounded-md placeholder:text-slate-400/90 focus:ring-4 focus:ring-primary focus:ring-opacity-20 focus:border-primary focus:border-opacity-40 dark:bg-darkmode-800 dark:border-transparent dark:focus:ring-slate-700 dark:focus:ring-opacity-50 dark:placeholder:text-slate-500/80" />
