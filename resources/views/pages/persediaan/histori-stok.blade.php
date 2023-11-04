3230b73017012f136f4f44f3d45ef4b939e23d21
Yasa Maulana <89472474+yasamaulana@users.noreply.github.com>
2023-11-04 21:08:52 +0700

    seetok

--- resources/views/pages/persediaan/histori-stok.blade.php
+++ resources/views/pages/persediaan/histori-stok.blade.php
@@ -1,15 +1,15 @@
 @extends('layout.main')
 
 @section('main')
-    <div class="intro-y flex items-center mt-8">
-        <h2 class="text-lg font-medium mr-auto">
+    <div class="flex items-center mt-8 intro-y">
+        <h2 class="mr-auto text-lg font-medium">
             Histori Stok
         </h2>
     </div>
     <div class="grid grid-cols-12 gap-6 mt-5">
-        <div class="intro-y col-span-12 flex flex-wrap sm:flex-nowrap items-center mt-2">
-            <div data-tw-merge class="block sm:flex items-center ml-5">
-                <label data-tw-merge for="horizontal-form-1" class="inline-block mb-2 font-bold mt-2 sm:w-20">
+        <div class="flex flex-wrap items-center col-span-12 mt-2 intro-y sm:flex-nowrap">
+            <div data-tw-merge class="items-center block ml-5 sm:flex">
+                <label data-tw-merge for="horizontal-form-1" class="inline-block mt-2 mb-2 font-bold sm:w-20">
                     Jenis
                 </label>
                 <select data-tw-merge aria-label="Default select example" class="form-control">
@@ -18,8 +18,8 @@
                     <option>Ahmad</option>
                 </select>
             </div>
-            <div data-tw-merge class="block sm:flex items-center ml-5">
-                <label data-tw-merge for="horizontal-form-1" class="inline-block mb-2 font-bold mt-2 sm:w-20">
+            <div data-tw-merge class="items-center block ml-5 sm:flex">
+                <label data-tw-merge for="horizontal-form-1" class="inline-block mt-2 mb-2 font-bold sm:w-20">
                     Rak
                 </label>
                 <select data-tw-merge aria-label="Default select example" class="form-control">
@@ -28,8 +28,8 @@
                     <option>Ahmad</option>
                 </select>
             </div>
-            <div data-tw-merge class="block sm:flex items-center ml-5">
-                <label data-tw-merge for="horizontal-form-1" class="inline-block mb-2 font-bold mt-2 sm:w-32">
+            <div data-tw-merge class="items-center block ml-5 sm:flex">
+                <label data-tw-merge for="horizontal-form-1" class="inline-block mt-2 mb-2 font-bold sm:w-32">
                     Sub Rak
                 </label>
                 <select data-tw-merge aria-label="Default select example" class="form-control">
@@ -41,18 +41,19 @@
         </div>
     </div>
     <!-- BEGIN: Data List -->
-    <div class="intro-y col-span-12 overflow-auto lg:overflow-visible">
-        <div class="intro-y box mt-5">
+    <div class="col-span-12 overflow-auto intro-y lg:overflow-visible">
+        <div class="mt-5 intro-y box">
             <table class="table mt-2">
                 <thead>
                     <tr>
                         <th class="whitespace-nowrap">No.</th>
-                        <th class="whitespace-nowrap">No. Reff</th>
-                        <th class="whitespace-nowrap">Tgl. Faktur</th>
-                        <th class="whitespace-nowrap">Suplier</th>
-                        <th class="whitespace-nowrap">Jatuh Tempo</th>
-                        <th class="whitespace-nowrap">Jumlah Piutang</th>
-                        <th class="whitespace-nowrap">Jenis Piutang</th>
+                        <th class="whitespace-nowrap">No. Produk</th>
+                        <th class="whitespace-nowrap">Tipe</th>
+                        <th class="whitespace-nowrap">Gudang</th>
+                        <th class="whitespace-nowrap">Rak</th>
+                        <th class="whitespace-nowrap">Sub Rak</th>
+                        <th class="whitespace-nowrap">Satuan Tekecil</th>
+                        <th class="whitespace-nowrap">Ket Satuan</th>
                         <th class="text-center whitespace-nowrap">ACTIONS</th>
                     </tr>
                 </thead>
@@ -65,8 +66,9 @@
                         <td class="">Contoh Data</td>
                         <td class="">Contoh Data</td>
                         <td class="">Contoh Data</td>
-                        <td class="table-report__action w-56">
-                            <div class="flex justify-center items-center">
+                        <td class="">Contoh Data</td>
+                        <td class="w-56 table-report__action">
+                            <div class="flex items-center justify-center">
                                 <a class="flex items-center text-success" href="javascript:;" data-tw-toggle="modal"
                                     data-tw-target="#cetak-kartu-stok"> <i data-feather="file-minus"
                                         class="w-4 h-4 mr-1"></i> Kartu Stok </a>
@@ -74,10 +76,10 @@
                                 <div id="cetak-kartu-stok" class="modal" tabindex="-1" aria-hidden="true">
                                     <div class="modal-dialog modal-xl">
                                         <div class="modal-content">
-                                            <div class="modal-header flex justify-end">
+                                            <div class="flex justify-end modal-header">
                                                 <button class="btn btn-warning btn-sm">Cetak Kartu Stok</button>
                                             </div>
-                                            <div class="modal-body p-5">
+                                            <div class="p-5 modal-body">
                                                 <table>
                                                     <tr>
                                                         <td>Nama Obat Produk </td>
@@ -110,15 +112,15 @@
                                                 </table>
                                                 <div class="flex justify-end">
                                                     <div
-                                                        class="w-56 relative text-slate-500 mt-3 border border-slate-600 rounded-lg">
-                                                        <input type="text" class="form-control w-56 box pr-10"
+                                                        class="relative w-56 mt-3 border rounded-lg text-slate-500 border-slate-600">
+                                                        <input type="text" class="w-56 pr-10 form-control box"
                                                             placeholder="Search...">
-                                                        <i class="w-4 h-4 absolute my-auto inset-y-0 mr-3 right-0"
+                                                        <i class="absolute inset-y-0 right-0 w-4 h-4 my-auto mr-3"
                                                             data-feather="search"></i>
                                                     </div>
                                                 </div>
                                                 <div class="overflow-x-auto">
-                                                    <table class="mt-5 table border border-slate-600">
+                                                    <table class="table mt-5 border border-slate-600">
                                                         <thead>
                                                             <tr>
                                                                 <td class="border border-slate-600">No</td>
